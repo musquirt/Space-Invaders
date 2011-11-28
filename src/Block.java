b8b04e2e140318e229db7ec46a97d94afb12c282
Tom Pollard <tpollard@purdue.edu>
2011-11-28 10:53:30 -0500

    Created graphics and .java files for blockades

---
+++ src/Block.java
@@ -0,0 +1,22 @@
+import com.brackeen.javagamebook.graphics.*;
+import java.awt.*;
+import javax.swing.ImageIcon;
+
+public class Block extends Sprite {
+
+	static Image hit0 = new ImageIcon("../graphics/0hit.png").getImage();
+	static Image hit1 = new ImageIcon("../graphics/1hit.png").getImage();
+	static Image hit2 = new ImageIcon("../graphics/2hit.png").getImage();
+	static Image hit3 = new ImageIcon("../graphics/3hit.png").getImage();
+	static Image hit4 = new ImageIcon("../graphics/4hit.png").getImage();
+
+	public Block( ) {
+	
+	
+	
+	}
+
+
+
+
+}
---
+++ src/Blockade.java
@@ -0,0 +1,11 @@
+import com.brackeen.javagamebook.graphics.*;
+import java.awt.*;
+import javax.swing.ImageIcon;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Blockade {
+
+
+
+}
